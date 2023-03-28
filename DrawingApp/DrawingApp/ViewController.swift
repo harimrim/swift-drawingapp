3f8ee619ff91aab003796b06b590ed4d036fa3c4
harimrim <kigkfla@naver.com>
2023-03-28 13:19:34 +0900

    프로젝트 생성
    drawingapp 프로젝트 생성 및 불필요한 주석 제거

---
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingApp
+//
+//  Created by 김하림 on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
